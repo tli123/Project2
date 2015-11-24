3d8118a1c2396e789664a704861d39d4f132f0fc
Tommy Li <tommyli12345@gmail.com>
2015-11-24 02:17:29 -0500

    update

--- Bank.java
+++ Bank.java
@@ -45,15 +45,134 @@ public class Bank extends Observable {
 	    String accountInfo = sc.next();
 	    String[] accountData = accountInfo.split(" ");
 	    Account acc = null;
-	    if (accountData[1].equals("c")) {
-		acc = new CDAccount(accountData[0], accountData[2], accountData[3]);
-	    } else if (accountData[1].equals("s")) {
-		acc = new SavingAccount(accountData[0], accountData[2], accountData[3]);
-	    } else if (accountData[1].equals("x")) {
-		acc = new CheckingAccount(accountData[0], accountData[2], accountData[3]);
+	    if (accountData[0].equals("c")) {
+		acc = new CDAccount(accountData[1], accountData[2], accountData[3]);
+	    } else if (accountData[0].equals("s")) {
+		acc = new SavingAccount(accountData[1], accountData[2], accountData[3]);
+	    } else if (accountData[0].equals("x")) {
+		acc = new CheckingAccount(accountData[1], accountData[2], accountData[3]);
 	    }
 	    accounts.add(acc);
 	}
+    }
+
+    public void batchMode(String filename) {
+	System.out.println("==========   Initial Bank Data ==================\n\n" + toString());
+	sc = new Scanner(new File(filename));
+	while (sc.hasNext()) {
+	    String command = sc.next();
+	    String[] commandData = command.split(" ");
+	    switch(commandData[0]){
+	    case "o":
+		open(commandData[1], Integer.parseInt(commandData[2]), Integer.parseInt(commandData[3]), Integer.parseInt(commandData[2]));
+	    case "c":
+		close(Integer.parseInt(commandData[1]));
+	    case "w":
+		withdraw(Integer.parseInt(commandData[1]), Integer.parseInt(commandData[2]));
+	    case "d":
+		deposit(Integer.parseInt(commandData[1]), Integer.parseInt(commandData[2]));
+	    case "a":
+		applyInterest();
+	    }
+	}
+	System.out.println("==========   Final Bank Data ==================\n\n" + toString());
+    }
+
+    private boolean contains(int id) {
+	for (Account accs : accounts) {
+	    if (accs.getID() == id) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public boolean open(String type, int id, int pin, int balance) {
+	if (!contains(id)) {
+	    Account acc = null;
+	    switch(type) {
+	    case "x":
+		if (balance < 0) {
+		    return false;
+		}
+                acc = new CheckingAccount(id, pin, balance);
+            case "s":
+		if (balance < 0) {
+		    return false;
+		}
+		acc = new SavingAccount(id, pin, balance);
+	    case "c":
+		if (balance < 500) {
+		    return false;
+		}
+		acc = new CDAccount(id, pin, balance);
+	    }
+	    acc.setOpen(true);
+	    account.add(acc);
+	} else {
+	    return false;
+	}
+    }
+
+    public boolean close(int id) {
+	if (!contains(id)) {
+	    return false;
+	} else {
+	    for (Account acc : accounts) {
+		if (acc.getID() == id) {
+		    acc.setOpen(false);
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    public boolean deposit(int id, int amount) {
+	if (contains(id)) {
+	    for (Account acc : accounts) {
+		if (acc.getID() == id) {
+		    if (acc.getOpen()) {
+			acc.modBalance(amount);
+			return true;
+		    } else {
+			return false;
+		    }
+		}
+	    }
+	}
+	return false;
+    }
+
+    public void withdraw(int id, int amount) {
+	if (contains(id)) {
+	    for (Account acc : accounts) {
+		if (acc.getID() == id) {
+		    if (acc.getOpen()) {
+			acc.withdraw(amount);
+			return true;
+		    } else {
+			return false;
+		    }
+		}
+	    }
+	}
+	return false;
+    }
+
+    public void applyInterest() {
+	
+	for (Account acc : accounts) {
+	    acc.applyMonthly();
+	}
+    }
+
+    public String toString(String type) {
+	String data += "Account Type    Account Balance\n------------    ------- -----------\n";
+	for (Account acc : accounts) {
+	    data += acc.formatReceipt() + "\n";
+	}
+	return data + "===============================================\n";
     }
 
 
@@ -66,7 +185,7 @@ public class Bank extends Observable {
 	if (args.length == 1) {
 	    //open gui
 	} else if (args.length == 2) {
-	    //open batchmode
+	    bank.batchMode();
 	}
     }
 
