8f2b2c3db834b9183724ed97b8d97fdf851a7755
Tommy Li <tommyli12345@gmail.com>
2015-11-24 03:45:25 -0500

    update

--- NegativeBalanceException.java
+++ NegativeBalanceException.java
@@ -23,8 +23,8 @@
 
 public class NegativeBalanceException extends Exception {
 
-    public NegativeBalanceException(String message) {
-	super(message);
+    public NegativeBalanceException() {
+	super();
     }
 
 }
