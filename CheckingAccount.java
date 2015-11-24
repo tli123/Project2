588b8c18642d1661499e1e81b6d7936b01ab2d88
Tommy Li <tommyli12345@gmail.com>
2015-11-24 03:53:22 -0500

    fixed constuctor

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -3,7 +3,7 @@ public class CheckingAccount extends Account {
     private static final double MIN_BALANCE = 50;
     private static final double INTEREST_RATE = 0;
 
-    public SavingAccount(int account_id, int pin, int balance) {
+    public SavingAccount(int account_id, int pin, double balance) {
 	super(account_id, pin, balance);
     }
 
--- SavingAccount.java
+++ SavingAccount.java
@@ -3,7 +3,7 @@ public class SavingAccount extends Account {
     private static final double MIN_BALANCE = 200;
     private static final double INTEREST_RATE = 0.005 / 12;
 
-    public SavingAccount(int account_id, int pin, int balance) {
+    public SavingAccount(int account_id, int pin, double balance) {
 	super(account_id, pin, balance);
     }
 
